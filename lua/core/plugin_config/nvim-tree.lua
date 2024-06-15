30c6075b38402d1a8e8d7b4963b3e98f162e6b02
tahseen.jamal <tahseen.jamal@gmail.com>
2024-06-15 23:48:46 +0400

    Added concept of file and directory without path
    Using small a for file and A for directory without showing any path

--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -5,9 +5,6 @@ vim.g.loaded_netrwPlugin = 1
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
 
--- empty setup using defaults
-require("nvim-tree").setup()
-
 -- OR setup with some options
 require("nvim-tree").setup({
   sort = {
@@ -22,6 +19,47 @@ require("nvim-tree").setup({
   filters = {
     dotfiles = true,
   },
+  on_attach = function(bufnr)
+    local api = require('nvim-tree.api')
+    local function opts(desc)
+      return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+
+    -- Default mappings
+    api.config.mappings.default_on_attach(bufnr)
+
+    -- Custom mapping to create a file
+    vim.keymap.set('n', 'a', function()
+      local node = api.tree.get_node_under_cursor()
+      local parent_dir = node.type == "directory" and node.absolute_path or vim.fn.fnamemodify(node.absolute_path, ":h")
+
+      vim.ui.input({ prompt = "Create file: " }, function(input)
+        if input then
+          local full_path = parent_dir .. "/" .. input
+          -- Create a file
+          vim.fn.writefile({}, full_path)
+          api.tree.reload()
+        end
+      end)
+    end, opts('Create File'))
+
+    -- Custom mapping to create a directory
+    vim.keymap.set('n', 'A', function()
+      local node = api.tree.get_node_under_cursor()
+      local parent_dir = node.type == "directory" and node.absolute_path or vim.fn.fnamemodify(node.absolute_path, ":h")
+
+      vim.ui.input({ prompt = "Create directory: " }, function(input)
+        if input then
+          local full_path = parent_dir .. "/" .. input
+          -- Remove the trailing slash and create directory
+          full_path = full_path:sub(1, -1)
+          vim.fn.mkdir(full_path, "p")
+          api.tree.reload()
+        end
+      end)
+    end, opts('Create Directory'))
+  end
 })
+
 
 vim.keymap.set('n', '<leader>e', ':NvimTreeFindFileToggle<CR>', { noremap = true, silent = true })
