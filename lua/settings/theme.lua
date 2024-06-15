c30dd6a815b27377fc9037195a2d9378339f9e0d
tahseen.jamal <tahseen.jamal@gmail.com>
2024-06-15 22:17:35 +0400

    Theme changed, ignore it

--- lua/settings/theme.lua
+++ lua/settings/theme.lua
@@ -1,5 +1,5 @@
 -- Themery block
 -- This block will be replaced by Themery.
-vim.cmd("colorscheme gruvbox")
-vim.g.theme_id = 1
+vim.cmd("colorscheme tokyonight")
+vim.g.theme_id = 3
 -- end themery block
