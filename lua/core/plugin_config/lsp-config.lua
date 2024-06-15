e094292d1b40ff9cec20008ee071ccfbd6c81b8c
tahseen.jamal <tahseen.jamal@gmail.com>
2024-06-15 22:46:58 +0400

    Resolved issue in the lsp config

---
+++ lua/core/plugin_config/lsp-config.lua
@@ -0,0 +1,105 @@
+-- lsp-config.lua
+
+-- Setup nvim-cmp.
+local cmp = require'cmp'
+
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        end,
+    },
+    mapping = {
+        ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+        ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+        ['<C-e>'] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+        }),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<C-j>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif require('luasnip').expand_or_jumpable() then
+                require('luasnip').expand_or_jump()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+        ['<C-k>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif require('luasnip').jumpable(-1) then
+                require('luasnip').jump(-1)
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+    },
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+    }, {
+        { name = 'buffer' },
+    })
+})
+
+-- Setup LSP servers
+local lspconfig = require'lspconfig'
+
+-- Go
+lspconfig.gopls.setup{
+    settings = {
+        gopls = {
+            analyses = {
+                unusedparams = true,
+            },
+            staticcheck = true,
+        },
+    },
+    on_attach = function(client, bufnr)
+        local function buf_set_option(...)
+            vim.api.nvim_buf_set_option(bufnr, ...)
+        end
+
+        -- Enable completion triggered by <c-x><c-o>
+        buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+        -- Enable auto-format and auto-import on save
+        if client.server_capabilities.documentFormattingProvider then
+            vim.cmd [[augroup Format]]
+            vim.cmd [[autocmd! * <buffer>]]
+            vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
+            vim.cmd [[autocmd BufWritePre <buffer> silent! lua vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })]]
+            vim.cmd [[augroup END]]
+        end
+    end,
+}
+
+-- Python
+lspconfig.pyright.setup{
+    settings = {
+        python = {
+            analysis = {
+                autoImportCompletions = true,
+            },
+        },
+    },
+}
+
+-- C/C++
+lspconfig.clangd.setup{
+    cmd = { "clangd", "--background-index" },
+    filetypes = { "c", "cpp", "objc", "objcpp" },
+    root_dir = lspconfig.util.root_pattern("compile_commands.json", "compile_flags.txt", ".git"),
+    settings = {
+        clangd = {
+            completion = {
+                enableSnippets = true,
+            },
+        },
+    },
+}
+
