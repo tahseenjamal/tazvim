fe6282b04274544d63dd37eb46880bee697e7f02
tahseen.jamal <tahseen.jamal@gmail.com>
2024-06-16 13:07:47 +0400

    Enhanced the configuration

--- lua/core/plugin_config/indentline.lua
+++ lua/core/plugin_config/indentline.lua
@@ -2,4 +2,15 @@ require("ibl").setup(
 {
       indent = { char = "│" },
       whitespace = { highlight = { "Whitespace", "NonText" } },
+      scope =  {
+        enabled = true,
+        show_start = true,
+        show_end = false,
+        injected_languages = false,
+        highlight = { "Function", "Label" },
+        priority = 500,
+        include = {
+          node_type = { ["*"] = { "*" } },    
+        },
+      },
 })
