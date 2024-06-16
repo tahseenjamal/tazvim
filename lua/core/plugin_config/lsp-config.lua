bbeefd01fd462fd1f8ceedb942458c5f0eadb7ca
tahseen.jamal <tahseen.jamal@gmail.com>
2024-06-16 14:30:41 +0400

    Added keymaps in C for fast action

--- lua/core/plugin_config/lsp-config.lua
+++ lua/core/plugin_config/lsp-config.lua
@@ -125,6 +125,18 @@ lspconfig.clangd.setup{
             },
         },
     },
+    -- Enable various python lsp features
+    on_attach = function(client, bufnr)
+      local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+      local opts = { noremap=true, silent=true }
+
+      -- Key mappings for LSP
+      buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+      buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+      buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+      buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+      buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    end
 }
 
 
