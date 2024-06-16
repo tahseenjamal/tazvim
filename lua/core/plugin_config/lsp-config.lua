a558716c21b1e2de19d760707873b35540d0ff16
tahseen.jamal <tahseen.jamal@gmail.com>
2024-06-16 20:45:45 +0400

    Added Rust and improved C/C++ configuration

--- lua/core/plugin_config/lsp-config.lua
+++ lua/core/plugin_config/lsp-config.lua
@@ -114,29 +114,127 @@ lspconfig.pyright.setup{
 }
 
 -- C/C++
-lspconfig.clangd.setup{
-    cmd = { "clangd", "--background-index" },
-    filetypes = { "c", "cpp", "objc", "objcpp" },
-    root_dir = lspconfig.util.root_pattern("compile_commands.json", "compile_flags.txt", ".git"),
-    settings = {
-        clangd = {
-            completion = {
-                enableSnippets = true,
-            },
-        },
-    },
-    -- Enable various python lsp features
+lspconfig.clangd.setup {
     on_attach = function(client, bufnr)
-      local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-      local opts = { noremap=true, silent=true }
+        local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+        local opts = { noremap=true, silent=true }
 
-      -- Key mappings for LSP
-      buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-      buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-      buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-      buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-      buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    end
+        buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+        buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+        buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+        buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+        buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+        buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+        buf_set_keymap('n', '<leader>cf', '<cmd>lua vim.lsp.buf.format({ async = true })<CR>', opts)
+
+        -- Setup auto import on save
+        if client.server_capabilities.documentFormattingProvider then
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                buffer = bufnr,
+                callback = function()
+                    vim.lsp.buf.format({ bufnr = bufnr })
+                end,
+            })
+        end
+    end,
+    cmd = { "clangd", "--background-index", "--suggest-missing-includes" },
+    init_options = {
+        clangdFileStatus = true,
+        usePlaceholders = true,
+        completeUnimported = true,
+        semanticHighlighting = true
+    },
+    flags = {
+        debounce_text_changes = 150,
+    }
 }
 
 
+-- Rust (rust-analyzer) setup
+lspconfig.rust_analyzer.setup {
+    on_attach = function(client, bufnr)
+        local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+        local opts = { noremap=true, silent=true }
+
+        buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+        buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+        buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+        buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+        buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    end,
+    flags = {
+        debounce_text_changes = 150,
+    },
+    settings = {
+      ["rust-analyzer"] = {
+        cargo = {
+            allFeatures = true
+        },
+        procMacro = {
+            enable = true
+        },
+        checkOnSave = {
+            command = "clippy"
+        },
+        diagnostics = {
+            enable = true,
+            disabled = { "unresolved-proc-macro" },
+            enableExperimental = true
+        },
+        completion = {
+            postfix = {
+                enable = true
+            },
+            autoimport = {
+                enable = true
+            }
+        },
+        inlayHints = {
+            typeHints = {
+                enable = true
+            },
+            chainingHints = {
+                enable = true
+            },
+            parameterHints = {
+                enable = true
+            },
+            closureReturnTypeHints = {
+                enable = true
+            }
+        },
+        imports = {
+            granularity = {
+                group = "module"
+            },
+            prefix = "self"
+        },
+        assist = {
+            importMergeBehavior = "last",
+            importPrefix = "by_self"
+        },
+        lens = {
+            enable = true,
+            references = {
+                enable = true
+            },
+            methodReferences = {
+                enable = true
+            },
+            enumVariantReferences = {
+                enable = true
+            }
+        },
+        rustfmt = {
+            enableRangeFormatting = true,
+            overrideCommand = {
+                "rustup", "run", "stable", "rustfmt", "--edition", "2021"
+            }
+        },
+        hoverActions = {
+            references = true,
+            implementation = true
+        }
+    }
+    }
+}
