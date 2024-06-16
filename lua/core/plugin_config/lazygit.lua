e53d594fd553f80812ccdf1d9df7620c94183e5e
tahseen.jamal <tahseen.jamal@gmail.com>
2024-06-16 21:23:46 +0400

    Modified for performance

--- lua/core/plugin_config/lazygit.lua
+++ lua/core/plugin_config/lazygit.lua
@@ -1,2 +1,16 @@
+vim.cmd [[
+    function! LazyGitToggle()
+        set lazyredraw
+        call termopen('lazygit', {'on_exit': function('s:OnExitLazyGit')})
+    endfunction
+
+    function! s:OnExitLazyGit(job_id, data, event)
+        set nolazyredraw
+    endfunction
+
+    command! LazyGitToggle call LazyGitToggle()
+]]
+
+
 vim.api.nvim_set_keymap('n', '<leader>gg', ':LazyGit<CR>', { noremap = true, silent = true })
 
