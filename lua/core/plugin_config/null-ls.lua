861c6346433b1558b1c4b33fc8f27a1a9c15da4d
tahseen.jamal <tahseen.jamal@gmail.com>
2024-06-16 13:09:04 +0400

    Formatting configuration

---
+++ lua/core/plugin_config/null-ls.lua
@@ -0,0 +1,19 @@
+-- Setup null-ls
+local null_ls = require('null-ls')
+
+null_ls.setup({
+  sources = {
+    null_ls.builtins.formatting.prettier.with({
+      extra_args = { "--single-quote", "--jsx-single-quote" }
+    }),
+    -- null_ls.builtins.formatting.gofmt,
+  },
+})
+
+-- Simplified autoformat on save
+vim.cmd([[
+  augroup FormatAutogroup
+    autocmd!
+    autocmd BufWritePost *.js,*.jsx,*.ts,*.tsx,*.json,*.html,*.css,*.md,*.go *.py lua vim.lsp.buf.format { async = true }
+  augroup END
+]])
