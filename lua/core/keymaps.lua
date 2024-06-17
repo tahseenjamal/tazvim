aa81a4a1f0359835c91de991764c745cca9a49f3
tahseen.jamal <tahseen.jamal@gmail.com>
2024-06-17 20:35:08 +0400

    Adjusted bufferline keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,3 +1,8 @@
+
+-- Hide tild (~) on the end of the buffers
+vim.opt.fillchars = { eob = ' ' }
+
+
 -- Enable absolute line numbers
 vim.wo.number = true
 -- Enable relative line numbers
@@ -12,6 +17,9 @@ vim.opt.laststatus = 2
 vim.opt.autowrite = true
 vim.opt.cursorline = true
 vim.opt.autoread = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 2
@@ -19,11 +27,17 @@ vim.opt.shiftround = true
 vim.opt.expandtab = true
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
--- Key mapping to delete the current buffer
-vim.api.nvim_set_keymap('n', '<leader>bd', ':bd<CR>', { noremap = true, silent = true })
 
+
+
+-- Key mapping to close the current buffer
+vim.api.nvim_set_keymap('n', '<leader>bd', ':bd<CR>', { noremap = true, silent = true })
 -- Key mapping to close all buffers
 vim.api.nvim_set_keymap('n', '<leader>ba', ':bufdo bd<CR>', { noremap = true, silent = true })
+
+-- Key mapping to navigate between buffers
+vim.api.nvim_set_keymap('n', '<leader>bb', ':bp<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>bn', ':bn<CR>', { noremap = true, silent = true })
 
 
 -- Code Action LSP
