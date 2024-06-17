115ee7bd3f524954884d30141812808a05e01f7d
tahseen.jamal <tahseen.jamal@gmail.com>
2024-06-17 20:51:40 +0400

    mason and mason lsp config

---
+++ lua/core/plugin_config/lsp-mason-config.lua
@@ -0,0 +1,17 @@
+-- Setup Mason
+require('mason').setup()
+
+-- Setup Mason LSPConfig
+require('mason-lspconfig').setup {
+  automatic_installation = true -- Automatically install configured servers
+}
+
+
+-- Automatically install any LSP server configured by lspconfig
+require('mason-lspconfig').setup_handlers {
+  function (server_name) -- default handler (optional)
+    lspconfig[server_name].setup {}
+  end
+}
+
+
