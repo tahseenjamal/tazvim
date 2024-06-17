5c078c0bc17f560a8dffcdba985b9c20802a2cf0
tahseen.jamal <tahseen.jamal@gmail.com>
2024-06-18 00:22:20 +0400

    added mason config in lsp config file

--- lua/core/plugin_config/lsp-config.lua
+++ lua/core/plugin_config/lsp-config.lua
@@ -1,6 +1,27 @@
 -- Setup LSP servers
 local lspconfig = require'lspconfig'
 
+-- Setup Mason
+require('mason').setup()
+
+-- Setup Mason LSPConfig
+require('mason-lspconfig').setup({
+  automatic_installation = true -- Automatically install configured servers
+})
+
+-- Automatically setup servers using mason-lspconfig
+require('mason-lspconfig').setup_handlers({
+  function(server_name)
+    lspconfig[server_name].setup {}
+  end,
+})
+
+-- Example LSP server setup
+local servers = { "gopls", "pyright", "tsserver" }
+for _, server in ipairs(servers) do
+  lspconfig[server].setup {}
+end
+
 -- Go
 lspconfig.gopls.setup{
     settings = {
