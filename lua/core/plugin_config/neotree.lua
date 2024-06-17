801d35e5c9221da07e10c210fb12e7bb0360e837
tahseen.jamal <tahseen.jamal@gmail.com>
2024-06-18 00:22:51 +0400

    created neo tree config file

---
+++ lua/core/plugin_config/neotree.lua
@@ -0,0 +1,20 @@
+require('neo-tree').setup({
+  -- popup_border_style = 'single',
+  -- auto_close = true,
+  -- update_focused_file = {
+  --   enable = true,
+  -- },
+  -- view = {
+  --   width = 20,
+  --   side = 'left',
+  --   auto_resize = false,
+  -- },
+  --  mappings = {
+  --   ["a"] = {
+  --       "add",
+  --       config = {
+  --         show_path = "none", -- "none", "relative", "absolute"
+  --       }
+  --     },
+  -- }
+})
