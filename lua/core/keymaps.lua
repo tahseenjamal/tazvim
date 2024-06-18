bed92c8f02982a42f710e405721f5bc07701f53a
tahseen.jamal <tahseen.jamal@gmail.com>
2024-06-18 11:50:48 +0400

    remapped buffer navigation to more accessible keys

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -28,6 +28,20 @@ vim.opt.expandtab = true
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 
+-- Function to close all buffers except the current one
+function CloseOtherBuffers()
+  local current_buf = vim.api.nvim_get_current_buf()
+  local buffers = vim.api.nvim_list_bufs()
+
+  for _, buf in ipairs(buffers) do
+    if buf ~= current_buf and vim.api.nvim_buf_is_loaded(buf) then
+      vim.cmd('bd ' .. buf)
+    end
+  end
+end
+
+-- Map the function to a key combination, e.g., <leader>bo (buffer other)
+vim.api.nvim_set_keymap('n', '<leader>bo', ':lua CloseOtherBuffers()<CR>', { noremap = true, silent = true })
 
 
 -- Key mapping to close the current buffer
@@ -36,8 +50,8 @@ vim.api.nvim_set_keymap('n', '<leader>bd', ':bd<CR>', { noremap = true, silent =
 vim.api.nvim_set_keymap('n', '<leader>ba', ':bufdo bd<CR>', { noremap = true, silent = true })
 
 -- Key mapping to navigate between buffers
-vim.api.nvim_set_keymap('n', '<leader>bb', ':bp<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>bn', ':bn<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>bf', ':bp<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>bj', ':bn<CR>', { noremap = true, silent = true })
 
 
 -- Code Action LSP
