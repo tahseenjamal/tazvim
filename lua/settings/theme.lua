b96a503094e876cf2253a177f9b66353cb131345
tahseen.jamal <tahseen.jamal@gmail.com>
2024-06-20 17:41:29 +0400

    theme changed

--- lua/settings/theme.lua
+++ lua/settings/theme.lua
@@ -1,5 +1,5 @@
 -- Themery block
 -- This block will be replaced by Themery.
-vim.cmd("colorscheme PaperColor")
-vim.g.theme_id = 7
+vim.cmd("colorscheme gruvbox8")
+vim.g.theme_id = 14
 -- end themery block
