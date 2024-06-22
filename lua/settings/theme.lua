a3ffafd64a435a724cb967f3bd07f81be7c885cb
tahseen.jamal <tahseen.jamal@gmail.com>
2024-06-22 20:31:33 +0400

    theme changed

--- lua/settings/theme.lua
+++ lua/settings/theme.lua
@@ -2,7 +2,7 @@
 -- This block will be replaced by Themery.
 vim.opt.background = "dark"
 
-vim.cmd("colorscheme caret")
-vim.g.theme_id = 31
+vim.cmd("colorscheme gruvbox")
+vim.g.theme_id = 1
 -- end themery block
 
