2a33488d2cf65097e4caba1414a0b5af42be9e57
tahseen.jamal <tahseen.jamal@gmail.com>
2024-06-22 09:09:27 +0400

    theme changed

--- lua/settings/theme.lua
+++ lua/settings/theme.lua
@@ -1,5 +1,5 @@
 -- Themery block
 -- This block will be replaced by Themery.
-vim.cmd("colorscheme nord")
-vim.g.theme_id = 3
+vim.cmd("colorscheme kanagawa-lotus")
+vim.g.theme_id = 12
 -- end themery block
