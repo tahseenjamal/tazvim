8ba837b3f1fb3812ecafca269844a3638ae044a1
tahseen.jamal <tahseen.jamal@gmail.com>
2024-06-23 09:40:26 +0400

    updated

---
+++ lua/core/plugin_config/hlchunk.lua
@@ -0,0 +1,14 @@
+require('hlchunk').setup({
+    chunk = {
+        enable = true,
+        disable_animation = true,
+        duration = 0,
+        delay = 0
+    },
+    indent = {
+        enable = true
+    },
+    line_num = {
+        enable = true
+    }
+})
