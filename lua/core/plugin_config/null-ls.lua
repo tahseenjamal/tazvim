e4b3b26d943da70b9d6b4a6471c1af66f3ebb28e
tahseen.jamal <tahseen.jamal@gmail.com>
2024-06-26 00:58:16 +0400

    updated

--- lua/core/plugin_config/null-ls.lua
+++ lua/core/plugin_config/null-ls.lua
@@ -1,7 +1,38 @@
 -- Setup null-ls
+-- local null_ls = require('null-ls')
+-- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+--
+-- null_ls.setup({
+--     sources = {
+--         null_ls.builtins.formatting.prettier.with({
+--             extra_args = { "--single-quote", "--jsx-single-quote" }
+--         }),
+--         -- null_ls.builtins.diagnostics.revive,  -- Use revive instead of golint
+--         null_ls.builtins.code_actions.gomodifytags,
+--         null_ls.builtins.code_actions.impl,
+--         null_ls.builtins.formatting.goimports_reviser,
+--         null_ls.builtins.formatting.gofumpt,
+--         null_ls.builtins.formatting.golines,
+--     },
+-- })
+--
+-- -- Simplified autoformat on save
+-- vim.cmd([[
+--   augroup FormatAutogroup
+--   autocmd!
+--   autocmd BufWritePost *.js,*.jsx,*.ts,*.tsx,*.json,*.html,*.css,*.md,*.go,*.py lua vim.lsp.buf.format { async = true }
+--   augroup END
+-- ]])
+--
+--
+--
+
 local null_ls = require('null-ls')
+
+-- Create a group for LSP formatting
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
+-- Setup null-ls
 null_ls.setup({
     sources = {
         null_ls.builtins.formatting.prettier.with({
@@ -14,13 +45,23 @@ null_ls.setup({
         null_ls.builtins.formatting.gofumpt,
         null_ls.builtins.formatting.golines,
     },
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function() vim.lsp.buf.format({ bufnr = bufnr, async = true }) end,
+            })
+        end
+    end,
 })
 
--- Simplified autoformat on save
-vim.cmd([[
-  augroup FormatAutogroup
-  autocmd!
-  autocmd BufWritePost *.js,*.jsx,*.ts,*.tsx,*.json,*.html,*.css,*.md,*.go,*.py lua vim.lsp.buf.format { async = true }
-  augroup END
-]])
 
+-- Simplified autoformat on save for various file types
+vim.api.nvim_create_augroup("FormatAutogroup", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePre", {
+    group = "FormatAutogroup",
+    pattern = "*.js,*.jsx,*.ts,*.tsx,*.json,*.html,*.css,*.md,*.go,*.py,*.c,*.cpp",
+    callback = function() vim.lsp.buf.format({ async = true }) end,
+})
