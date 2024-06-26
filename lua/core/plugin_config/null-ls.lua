7793d0e1129c0ac3d6d6adabce50b77144bba1ba
tahseen.jamal <tahseen.jamal@gmail.com>
2024-06-26 19:00:39 +0400

    updated

--- lua/core/plugin_config/null-ls.lua
+++ lua/core/plugin_config/null-ls.lua
@@ -1,31 +1,3 @@
--- Setup null-ls
--- local null_ls = require('null-ls')
--- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
---
--- null_ls.setup({
---     sources = {
---         null_ls.builtins.formatting.prettier.with({
---             extra_args = { "--single-quote", "--jsx-single-quote" }
---         }),
---         -- null_ls.builtins.diagnostics.revive,  -- Use revive instead of golint
---         null_ls.builtins.code_actions.gomodifytags,
---         null_ls.builtins.code_actions.impl,
---         null_ls.builtins.formatting.goimports_reviser,
---         null_ls.builtins.formatting.gofumpt,
---         null_ls.builtins.formatting.golines,
---     },
--- })
---
--- -- Simplified autoformat on save
--- vim.cmd([[
---   augroup FormatAutogroup
---   autocmd!
---   autocmd BufWritePost *.js,*.jsx,*.ts,*.tsx,*.json,*.html,*.css,*.md,*.go,*.py lua vim.lsp.buf.format { async = true }
---   augroup END
--- ]])
---
---
---
 
 local null_ls = require('null-ls')
 
