c375f9c45de0d479d133ca6720b0ab8b78304bb0
tahseen.jamal <tahseen.jamal@gmail.com>
2024-06-28 18:19:43 +0400

    configuration for line jump

--- lua/core/plugin_config/flash.lua
+++ lua/core/plugin_config/flash.lua
@@ -25,10 +25,19 @@ require('packer').startup(function()
   }
 end)
 
--- Keybindings for flash.nvim
+-- Configure flash.nvim
+require("flash").jump({
+    search = { mode = "search", max_length = 0 },
+    label = { after = { 0, 0 } },
+    pattern = "^"
+})
+
+local opts = { noremap = true, silent = true }
+
+-- Key mapping 
+vim.api.nvim_set_keymap('n', '<leader>l', ':lua require("flash").jump({search = {mode = "search", max_length = 0}, label = {after = {0, 0}}, pattern = "^"})<CR>', opts)
 vim.keymap.set('n', '<C-s>', function() require("flash").jump() end, { noremap = true, silent = true, desc = "Flash Jump" })
 vim.keymap.set('n', '<C-S-s>', function() require("flash").treesitter() end, { noremap = true, silent = true, desc = "Flash Treesitter" })
 vim.keymap.set('o', '<C-r>', function() require("flash").remote() end, { noremap = true, silent = true, desc = "Remote Flash" })
 vim.keymap.set('o', '<C-S-r>', function() require("flash").treesitter_search() end, { noremap = true, silent = true, desc = "Treesitter Search" })
 vim.keymap.set('c', '<C-s>', function() require("flash").toggle() end, { noremap = true, silent = true, desc = "Toggle Flash Search" })
-vim.api.nvim_set_keymap('n', '<leader>l', ":lua require'flash'.jump({mode='line'})<CR>", { noremap = true, silent = true })
