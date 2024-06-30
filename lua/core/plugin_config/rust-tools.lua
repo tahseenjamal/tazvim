00e4c943b0672e2fe2003179ac749fa02fa359e5
tahseen.jamal <tahseen.jamal@gmail.com>
2024-06-30 10:23:15 +0400

    some minor additions

--- lua/core/plugin_config/rust-tools.lua
+++ lua/core/plugin_config/rust-tools.lua
@@ -1,12 +1,21 @@
 local rt = require("rust-tools")
 
 rt.setup({
-  server = {
-    on_attach = function(_, bufnr)
-      -- Hover actions
-      vim.keymap.set("n", "<leader>rt", rt.hover_actions.hover_actions, { buffer = bufnr })
-      -- Code action groups
-      vim.keymap.set("n", "<leader>at", rt.code_action_group.code_action_group, { buffer = bufnr })
-    end,
-  },
+    tools = {
+        autoSetHints = true,
+        inlay_hints = {
+            show_parameter_hints = true,
+            parameter_hints_prefix = "<- ",
+            other_hints_prefix = "=> ",
+            highlight = "Comment",
+        },
+    },
+    server = {
+        on_attach = function(_, bufnr)
+            -- Hover actions
+            vim.keymap.set("n", "<leader>rt", rt.hover_actions.hover_actions, { buffer = bufnr })
+            -- Code action groups
+            vim.keymap.set("n", "<leader>at", rt.code_action_group.code_action_group, { buffer = bufnr })
+        end,
+    },
 })
