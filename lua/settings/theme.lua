4fd9052964d0e171905be9b0fe0a3d49bfb08093
tahseen.jamal <tahseen.jamal@gmail.com>
2024-07-01 20:13:46 +0400

    updated

--- lua/settings/theme.lua
+++ lua/settings/theme.lua
@@ -1,8 +1,8 @@
 -- Themery block
 -- This block will be replaced by Themery.
-vim.opt.background = "dark"
+vim.g.catppuccin_flavour = "frappe"
 
-vim.cmd("colorscheme gruvbox")
-vim.g.theme_id = 1
+vim.cmd("colorscheme catppuccin")
+vim.g.theme_id = 5
 -- end themery block
 
