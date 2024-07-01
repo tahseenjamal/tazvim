e6aa116952b531fa1d56bc7708f02bb8feee6e1c
tahseen.jamal <tahseen.jamal@gmail.com>
2024-07-01 20:13:23 +0400

    updated

--- lua/core/plugin_config/inlay-hints.lua
+++ lua/core/plugin_config/inlay-hints.lua
@@ -1,16 +1,42 @@
 local inlay_hints = require("inlay-hints")
 
 inlay_hints.setup({
-    commands = { enable = true },
-    autocmd = { enable = true },
-    options = {
-        highlight = "Comment",
-        prefix = " » ",
-        aligned = false,
-        only_current_line = false,
-        enabled = { "TypeHint", "ChainingHint", "ParameterHint" },
+  commands = { 
+    enable = true 
+  },
+  autocmd = { 
+    enable = true 
+  },
+  options = {
+    highlight = "Comment",
+    prefix = " » ",
+    aligned = false,
+    only_current_line = false,
+    show_parameter_hints = true,
+    show_type_hints = true,
+    enabled = { "TypeHint", "ChainingHint", "ParameterHint" },
+    disable_virtual_text = false,  
+    max_len_align = false,         
+    right_align = false,           
+    right_align_padding = 7,       
+    hint_formatters = {
+      TypeHint = function(hint)
+        return "=> " .. hint
+      end,
+      ParameterHint = function(hint)
+        return "param: " .. hint
+      end,
+      ChainingHint = function(hint)
+        return "chain: " .. hint
+      end,
     },
+  },
+  languages = {
+    rust = {
+      enabled = {"TypeHint", "ParameterHint"},
+      only_current_line = false,
+      prefix = " -> ",
+    },
+  },
 })
 
--- Define the keymap for enabling inlay hints
-vim.api.nvim_set_keymap('n', '<leader>h', ':lua vim.lsp.inlay_hint.enable()<CR>', { noremap = true, silent = true })
