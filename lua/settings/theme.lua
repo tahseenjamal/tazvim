2ecc65a55c9ca3660d18fad7d9c5add1def604dd
tahseen.jamal <tahseen.jamal@gmail.com>
2024-07-03 11:18:38 +0400

    updated

--- lua/settings/theme.lua
+++ lua/settings/theme.lua
@@ -1,8 +1,8 @@
 -- Themery block
 -- This block will be replaced by Themery.
-vim.g.catppuccin_flavour = "frappe"
+vim.opt.background = "dark"
 
-vim.cmd("colorscheme catppuccin")
-vim.g.theme_id = 5
+vim.cmd("colorscheme evergarden")
+vim.g.theme_id = 12
 -- end themery block
 
