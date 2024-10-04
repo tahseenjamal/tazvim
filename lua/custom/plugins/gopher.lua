af272f66b8e143114465c7ed9e748438674e4b33
tahseen.jamal <tahseen.jamal@gmail.com>
2024-10-04 13:31:57 +0530

    Correction

--- lua/custom/plugins/gopher.lua
+++ lua/custom/plugins/gopher.lua
@@ -10,7 +10,7 @@ return {
   },
   -- (optional) will update plugin's deps on every update
   build = function()
-    vim.cmd.GoInstallDeps()
+    vim.cmd.('GoInstallDeps')
   end,
   opts = {},
   config = function()
