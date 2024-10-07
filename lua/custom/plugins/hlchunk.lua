e7eef61f2af05ff45f86ce3a3d274ff82e88375f
tahseen.jamal <tahseen.jamal@gmail.com>
2024-10-07 20:41:50 +0400

    Updates

--- lua/custom/plugins/hlchunk.lua
+++ lua/custom/plugins/hlchunk.lua
@@ -1,19 +1,19 @@
 return {
-  'shellRaining/hlchunk.nvim',
-  config = function()
-    require('hlchunk').setup {
-      chunk = {
-        enable = true,
-        disable_animation = true,
-        duration = 0,
-        delay = 0,
-      },
-      indent = {
-        enable = true,
-      },
-      line_num = {
-        enable = true,
-      },
-    }
-  end,
+	"shellRaining/hlchunk.nvim",
+	config = function()
+		require("hlchunk").setup({
+			chunk = {
+				enable = true,
+				disable_animation = true,
+				duration = 0,
+				delay = 0,
+			},
+			indent = {
+				enable = true,
+			},
+			line_num = {
+				enable = true,
+			},
+		})
+	end,
 }
--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -1,59 +1,99 @@
 return { -- Collection of various small independent plugins/modules
-  'echasnovski/mini.nvim',
-  config = function()
-    -- Better Around/Inside textobjects
-    --
-    -- Examples:
-    --  - va)  - [V]isually select [A]round [)]paren
-    --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-    --  - ci'  - [C]hange [I]nside [']quote
-    require('mini.ai').setup { n_lines = 500 }
+	"echasnovski/mini.nvim",
+	config = function()
+		-- Better Around/Inside textobjects
+		--
+		-- Examples:
+		--  - va)  - [V]isually select [A]round [)]paren
+		--  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+		--  - ci'  - [C]hange [I]nside [']quote
+		require("mini.ai").setup({ n_lines = 500 })
 
-    -- Add/delete/replace surroundings (brackets, quotes, etc.)
-    --
-    -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-    -- - sd'   - [S]urround [D]elete [']quotes
-    -- - sr)'  - [S]urround [R]eplace [)] [']
-    require('mini.surround').setup()
+		-- Add/delete/replace surroundings (brackets, quotes, etc.)
+		--
+		-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+		-- - sd'   - [S]urround [D]elete [']quotes
+		-- - sr)'  - [S]urround [R]eplace [)] [']
+		require("mini.surround").setup( -- No need to copy this inside `setup()`. Will be used automatically.
+			{
+				-- Add custom surroundings to be used on top of builtin ones. For more
+				-- information with examples, see `:h MiniSurround.config`.
+				custom_surroundings = nil,
 
-    require('mini.move').setup {
-      mappings = {
-        -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
-        left = '<C-h>',
-        right = '<C-l>',
-        down = '<C-j>',
-        up = '<C-k>',
+				-- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
+				highlight_duration = 500,
 
-        -- Move current line in Normal mode
-        line_left = '<C-h>',
-        line_right = '<C-l>',
-        line_down = '<C-j>',
-        line_up = '<C-k>',
-      },
+				-- Module mappings. Use `''` (empty string) to disable one.
+				mappings = {
+					add = "ya", -- Add surrounding in Normal and Visual modes
+					delete = "yd", -- Delete surrounding
+					find = "yf", -- Find surrounding (to the right)
+					find_left = "yF", -- Find surrounding (to the left)
+					highlight = "yh", -- Highlight surrounding
+					replace = "yr", -- Replace surrounding
+					update_n_lines = "yn", -- Update `n_lines`
 
-      -- Options which control moving behavior
-      options = {
-        -- Automatically reindent selection during linewise vertical move
-        reindent_linewise = true,
-      },
-    }
+					suffix_last = "l", -- Suffix to search with "prev" method
+					suffix_next = "n", -- Suffix to search with "next" method
+				},
 
-    -- Simple and easy statusline.
-    --  You could remove this setup call if you don't like it,
-    --  and try some other statusline plugin
-    -- local statusline = require 'mini.statusline'
-    -- set use_icons to true if you have a Nerd Font
-    -- statusline.setup { use_icons = vim.g.have_nerd_font }
+				-- Number of lines within which surrounding is searched
+				n_lines = 20,
 
-    -- You can configure sections in the statusline by overriding their
-    -- default behavior. For example, here we set the section for
-    -- cursor location to LINE:COLUMN
-    ---@diagnostic disable-next-line: duplicate-set-field
-    -- statusline.section_location = function()
-    --   return '%2l:%-2v'
-    -- end
+				-- Whether to respect selection type:
+				-- - Place surroundings on separate lines in linewise mode.
+				-- - Place surroundings on each line in blockwise mode.
+				respect_selection_type = false,
 
-    -- ... and there is more!
-    --  Check out: https://github.com/echasnovski/mini.nvim
-  end,
+				-- How to search for surrounding (first inside current line, then inside
+				-- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
+				-- 'cover_or_nearest', 'next', 'prev', 'nearest'. For more details,
+				-- see `:h MiniSurround.config`.
+				search_method = "cover",
+
+				-- Whether to disable showing non-error feedback
+				silent = false,
+			}
+		)
+
+		require("mini.move").setup({
+			mappings = {
+				-- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+				left = "<C-h>",
+				right = "<C-l>",
+				down = "<C-j>",
+				up = "<C-k>",
+
+				-- Move current line in Normal mode
+				line_left = "<C-h>",
+				line_right = "<C-l>",
+				line_down = "<C-j>",
+				line_up = "<C-k>",
+			},
+
+			-- Options which control moving behavior
+			options = {
+				-- Automatically reindent selection during linewise vertical move
+				reindent_linewise = true,
+			},
+		})
+
+		-- Simple and easy statusline.
+		--  You could remove this setup call if you don't like it,
+		--  and try some other statusline plugin
+		-- local statusline = require 'mini.statusline'
+		-- set use_icons to true if you have a Nerd Font
+		-- statusline.setup { use_icons = vim.g.have_nerd_font }
+
+		-- You can configure sections in the statusline by overriding their
+		-- default behavior. For example, here we set the section for
+		-- cursor location to LINE:COLUMN
+		---@diagnostic disable-next-line: duplicate-set-field
+		-- statusline.section_location = function()
+		--   return '%2l:%-2v'
+		-- end
+
+		-- ... and there is more!
+		--  Check out: https://github.com/echasnovski/mini.nvim
+	end,
 }
